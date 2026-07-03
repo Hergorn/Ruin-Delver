5256ed98f26766cfb10c156fa05577e1114b2b2f
Hergorn <hergorntv@friesencrew.de>
2026-07-03 16:38:10 +0200

    platform pathing learned

--- Scripts/moveable_platform.gd
+++ Scripts/moveable_platform.gd
@@ -5,6 +5,10 @@ extends Node2D
 @export_enum("vertical", "horizontal") var orientation : String
 @export var needsSwitch : bool
 @export var id : int
+@export_enum("vertical", "horizontal") var pathOrientation : String
+@export var pathLength : int
+@export var customPath : bool
+@export var speed : float = 0.2
 
 @onready var hide : Node2D = $hidden
 @onready var platformBody : Node2D = $platformBody
@@ -12,18 +16,23 @@ extends Node2D
 @onready var middlePart : StaticBody2D = $hidden/middlePart
 @onready var lastPart : StaticBody2D = $hidden/lastPart
 @onready var singlePart : StaticBody2D = $hidden/singlePart
-
-##testing
-@onready var pathfollow : PathFollow2D = $TEST/Path2D/PathFollow2D
-@onready var speed : float = 0.2
+@onready var pathfollow : PathFollow2D = $PathFollow
 @onready var forwardDirection = 1
-##testing
+@onready var path : Path2D = $"."
+@onready var crystal = preload("res://Scenes/crystal.tscn").instantiate()
 
 func _ready() -> void:
 	hide.visible = false
-	for child in hide.get_children(): child.position = Vector2(0,0)
+	for child in hide.get_children(): 
+		child.position.x = 0
+		child.position.y = 0
 	aufbau()
+	if customPath == false: createPath()
+	if needsSwitch == true: platformBody.add_child(crystal)
+
 		
+	
+
 func aufbau():
 	if length == 0: length = 1
 	var newPart : StaticBody2D
@@ -40,14 +49,13 @@ func aufbau():
 					1: newPart =  lastPart.duplicate()
 					_: newPart =  middlePart.duplicate()
 				setPartPosition(newPart, i)
-				print(newPart.position)
 				platformBody.add_child(newPart)
 
 func setPartPosition(part : StaticBody2D, i : int):
 	match orientation:
-		"horizontal": part.position.x = i * 48
+		"horizontal": part.position = Vector2 (i * 48, 0)
 		"vertical": 
-			part.position.y = i * 48 
+			part.position = Vector2(0, i * 48)
 			part.rotation = deg_to_rad(90.0)
 
 func _process(delta: float) -> void:
@@ -60,3 +68,10 @@ func _physics_process(delta: float) -> void:
 		forwardDirection = -1
 	elif forwardDirection == -1 and pathfollow.progress_ratio == 0: 
 		forwardDirection = 1
+	print(platformBody.position)
+
+func createPath():
+	for i in pathLength:
+		match pathOrientation:
+			"horizontal":  path.curve.add_point(Vector2(i*48,0))
+			"vertical": path.curve.add_point(Vector2(0,i*-48))
